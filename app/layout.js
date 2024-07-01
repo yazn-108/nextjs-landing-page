97b05364dc1aee178bd18a7fb5199ed225d32ae9
yazn-108 <gamesalab22@gmail.com>
2024-07-01 20:41:20 +0300

    nextjs landing page

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,11 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
-  title: "yazn_108 | next.js 14",
+  title: "yazn_108 | next.js landing page",
   description: "nextjs & framer-motion & tailwind",
   openGraph: {
     url: "https://yazn-landing-nextjs.vercel.app/",
-    title: "yazn_108 | next.js 14",
+    title: "yazn_108 | next.js landing page",
     description: "nextjs & framer-motion & tailwind",
     images: [
       {
@@ -22,7 +22,7 @@ export const metadata = {
     card: "summary_large_image",
     domain: "yazn-108.github.io",
     url: "https://yazn-landing-nextjs.vercel.app/",
-    title: "yazn_108 | next.js 14",
+    title: "yazn_108 | next.js landing page",
     description: "nextjs & framer-motion & tailwind",
     image:
       "https://raw.githubusercontent.com/yazn-108/nextjs-landing-page/main/siteBanner.png",
